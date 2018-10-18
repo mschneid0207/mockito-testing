b1499e40b41dd202aa10715781ab5b7dc45c7160
Markus Schneider <markus.schneider0207@gmail.com>
2018-10-18 22:36:04 +0200

    multiple return values

---
+++ src/test/java/de/mschneid/unittesting/business/ListMockTest.java
@@ -0,0 +1,34 @@
+package de.mschneid.unittesting.business;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class ListMockTest {
+
+    List mock = mock(List.class);
+
+    @Test
+    public void size_basic() {
+        when(mock.size()).thenReturn(5);
+        assertEquals(5, mock.size());
+    }
+
+    @Test
+    public void returnDifferentValues() {
+        when(mock.size()).thenReturn(5).thenReturn(10);
+        assertEquals(5, mock.size());
+        assertEquals(10, mock.size());
+    }
+
+    @Test
+    public void returnWithParameters() {
+       when(mock.get(0)).thenReturn("test");
+       assertEquals("test", mock.get(0));
+        assertEquals(null, mock.get(1));
+    }
+}
