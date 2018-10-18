b97a6342e2047e0876b58ce75f801825cf7426f1
Markus Schneider <markus.schneider0207@gmail.com>
2018-10-18 22:16:33 +0200

    changed to InjectMocks and Mock annotation

--- src/test/java/de/mschneid/unittesting/business/SomeBusinessMockTest.java
+++ src/test/java/de/mschneid/unittesting/business/SomeBusinessMockTest.java
@@ -3,22 +3,24 @@ package de.mschneid.unittesting.business;
 import de.mschneid.unittesting.data.SomeDataService;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
+@RunWith(MockitoJUnitRunner.class)
 public class SomeBusinessMockTest {
 
-    SomeBusinessImpl business = new SomeBusinessImpl();
-    SomeDataService dataServiceMock = mock(SomeDataService.class);
-
-    @Before
-    public void setUp() {
-        business.setSomeDataService(dataServiceMock);
-    }
+    @InjectMocks
+    SomeBusinessImpl business;
+    @Mock
+    SomeDataService dataServiceMock;
 
     @Test
     public void calculateSumUsingDataService_basic() {
