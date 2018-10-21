fe06be216c2e19325cfe48012109c12d88b09b9b
Markus Schneider <markus.schneider0207@gmail.com>
2018-10-21 23:21:36 +0200

    test for item business service

---
+++ src/test/java/de/mschneid/unittesting/service/ItemBusinessServiceTest.java
@@ -0,0 +1,41 @@
+package de.mschneid.unittesting.service;
+
+import de.mschneid.unittesting.data.ItemRepository;
+import de.mschneid.unittesting.model.Item;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ItemBusinessServiceTest {
+
+    @InjectMocks
+    private ItemBusinessService sut;
+
+    @Mock
+    private ItemRepository repositoryMock;
+
+    @Test
+    public void retrieveAllItems() {
+        Item item = Item.builder().id(1).name("Ball").price(10).quantity(20).build();
+        Item item2 = Item.builder().id(2).name("Rad").price(200).quantity(2).build();
+
+        when(repositoryMock.findAll()).thenReturn(Arrays.asList(item, item2));
+
+        List<Item> items = sut.retrieveAllItems();
+        Assert.assertEquals(200, items.get(0).getValue());
+        Assert.assertEquals(400, items.get(1).getValue());
+
+    }
+
+}
