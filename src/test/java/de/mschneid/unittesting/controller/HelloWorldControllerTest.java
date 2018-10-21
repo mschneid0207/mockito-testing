c569090c103d26612fcfd8e16d9a36186a19669b
Markus Schneider <markus.schneider0207@gmail.com>
2018-10-21 18:37:58 +0200

    spring mock mvc to test a controller

---
+++ src/test/java/de/mschneid/unittesting/controller/HelloWorldControllerTest.java
@@ -0,0 +1,35 @@
+package de.mschneid.unittesting.controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = HelloWorldController.class)
+public class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void helloWorld_basic() throws Exception {
+        RequestBuilder request =
+                MockMvcRequestBuilders
+                .get("/hello-world")
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(request).andReturn();
+
+        assertEquals("Hello World", result.getResponse().getContentAsString());
+    }
+
+}
